e061e59c62c5d8b2d4e8af313df846e8bdd84171
Bruno Mesquita dos Santos <45662184+BrunoMesquitaa@users.noreply.github.com>
2024-09-22 05:19:34 -0300

    Update deploy.R

--- deploy.R
+++ deploy.R
@@ -1,4 +1,11 @@
 if (!require('rsconnect'))install.packages("rsconnect");library(rsconnect)
+if (!require('BiocManager'))install.packages("BiocManager");library(BiocManager)
+
+options(
+  repos = c(CRAN_mirror = "https://cran.rstudio.com/"),
+  BioC_mirror = "https://bioconductor.statistik.tu-dortmund.de/"
+)
+options(repos = BiocManager::repositories())
 
 rsconnect::setAccountInfo(name=Sys.getenv("NAME"), 
                           token=Sys.getenv("TOKEN"), 
