b6522a7ba6f87df71b320306e8ef6cba804eb966
Bruno Mesquita dos Santos <45662184+BrunoMesquitaa@users.noreply.github.com>
2024-09-22 04:34:18 -0300

    add shiny deploy teste

---
+++ deploy.R
@@ -0,0 +1,7 @@
+if (!require('rsconnect'))install.packages("rsconnect");library(rsconnect)
+
+rsconnect::setAccountInfo(name=Sys.getenv("NAME"), 
+                          token=Sys.getenv("TOKEN"), 
+                          secret=Sys.getenv("SECRET"))
+
+deployApp()
