4f1726f7bf482638525e140fd334008a2ea17f71
Bruno Mesquita dos Santos <45662184+BrunoMesquitaa@users.noreply.github.com>
2024-09-22 14:28:05 -0300

    testes

--- deploy.R
+++ deploy.R
@@ -1,11 +1,5 @@
 if (!require('rsconnect'))install.packages("rsconnect");library(rsconnect)
 
-options(
-  repos = c(CRAN_mirror = "https://cran.rstudio.com/"),
-  BioC_mirror = "https://bioconductor.statistik.tu-dortmund.de/"
-)
-
-
 rsconnect::setAccountInfo(name=Sys.getenv("NAME"), 
                           token=Sys.getenv("TOKEN"), 
                           secret=Sys.getenv("SECRET"))
