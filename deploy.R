9dfb5fb0823f5c7c8de9820226d9d160032f15f0
Bruno Mesquita dos Santos <45662184+BrunoMesquitaa@users.noreply.github.com>
2024-10-01 19:51:24 -0300

    apresentacao

--- deploy.R
+++ deploy.R
@@ -4,4 +4,4 @@ rsconnect::setAccountInfo(name=Sys.getenv("NAME"),
                           token=Sys.getenv("TOKEN"), 
                           secret=Sys.getenv("SECRET"))
 
-deployApp(forceUpdate = TRUE)
+deployApp(forceUpdate = TRUE, quarto = TRUE)
